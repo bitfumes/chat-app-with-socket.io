4c404d8df65734837c485e5a3620b9458471e459
Bitfumes <bitfumes@gmail.com>
2018-05-08 18:02:39 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,44 @@
+let app = require('express')();
+
+let http = require('http').Server(app);
+
+let io = require('socket.io')(http);
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+})
+
+io.on('connection', (socket) => {
+    io.emit('noOfConnections', Object.keys(io.sockets.connected).length)
+
+
+    socket.on('disconnect', () => {
+        console.log('disconnected')
+        io.emit('noOfConnections', Object.keys(io.sockets.connected).length)
+    })
+
+
+
+    socket.on('chat-message', (msg) => {
+        socket.broadcast.emit('chat-message', msg)
+    })
+    socket.on('joined', (name) => {
+        io.emit('joined')
+    })
+    socket.on('leaved', (name) => {
+        socket.broadcast.emit('leaved', name)
+    })
+
+    socket.on('typing', (data) => {
+        socket.broadcast.emit('typing', data)
+    })
+    socket.on('stoptyping', () => {
+        socket.broadcast.emit('stoptyping')
+    })
+
+
+})
+
+http.listen(3000, () => {
+    console.log('Server is started at http://localhost:3000')
+})
